e9c3311b4986bdbd2785c150261c7f0013f659b6
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-11 18:35:52 +0530

    Create PSScript.ps1

---
+++ PSScript.ps1
@@ -0,0 +1,4 @@
+$url = "https://notepad-plus-plus.org/repository/7.x/7.6/npp.7.6.Installer.x64.exe"
+$output = "C:\Program Files\Notepad++\npp.exe"
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url -Destination $output} -Credential $cred 
