6a40e2f5d10ff72e8721366630f4e6515dcec3ed
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-12 16:58:12 +0530

    Update downloadfile.ps1

--- Scripts/downloadfile.ps1
+++ Scripts/downloadfile.ps1
@@ -1,4 +1,4 @@
 $url = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117"
-$output = "C:/Users/vmadmin/Desktop/npp.exe"
+$output = "C:/Users/vmadmin/Desktop/odt.exe"
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
