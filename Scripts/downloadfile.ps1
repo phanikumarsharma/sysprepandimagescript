50d0ffd1842aa1ac1b09b9f38cd094b5ab923a44
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-12 16:09:31 +0530

    Update downloadfile.ps1

--- Scripts/downloadfile.ps1
+++ Scripts/downloadfile.ps1
@@ -1,4 +1,4 @@
 $url = "https://notepad-plus-plus.org/repository/7.x/7.6/npp.7.6.Installer.x64.exe"
 $output = "C:/Users/vmadmin/Desktop/npp.exe"
 Import-Module BitsTransfer
-Start-BitsTransfer -Source $url -Destination $output} -Credential $cred
+Start-BitsTransfer -Source $url -Destination $output
