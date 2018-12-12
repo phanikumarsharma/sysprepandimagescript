9afec24e92997db04fbbb3955be91eb4d96f5651
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-12 15:29:14 +0530

    Update downloadfile.ps1

--- Scripts/downloadfile.ps1
+++ Scripts/downloadfile.ps1
@@ -1,4 +1,4 @@
-$url = "https://www.mozilla.org/en-US/firefox/download/thanks/"
-$output = "C:/Users/vmadmin/Desktop/firefox.exe"
+$url = "https://notepad-plus-plus.org/repository/7.x/7.6/npp.7.6.Installer.x64.exe"
+$output = "C:/Users/vmadmin/Desktop/npp.exe"
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output} -Credential $cred
