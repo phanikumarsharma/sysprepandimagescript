b4b0c94bbbcd1c359167d0d38e9c43c1241767eb
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-12 11:43:52 +0530

    Update sysprep.ps1

--- Scripts/sysprep.ps1
+++ Scripts/sysprep.ps1
@@ -2,9 +2,9 @@ param([switch]$runSysprep=$false)
 write-output "Sysprep Script Run, parameter 'runSysprep': $runSysprep"
 
 if($runSysprep){
-  write-output "starting Sysprep"
+  write-output "starting the Sysprep"
   Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /shutdown /quiet'
   write-output "started Sysprep"
 }else{
-  write-output "skipping Sysprep"
+  write-output "skipping the Sysprep"
 }
