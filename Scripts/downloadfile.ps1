a4a6cd5469eced505da32336c86eb54cea421564
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-13 13:41:55 +0530

    Update downloadfile.ps1

--- Scripts/downloadfile.ps1
+++ Scripts/downloadfile.ps1
@@ -2,3 +2,5 @@ $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EB
 $output = "C:\Program Files\odt.exe"
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
+#to extract the ODT tool
+Start-Process -FilePath "C:\Program Files\odt.exe" -ArgumentList '/extract:"C:\Phani\Extracted" /quiet'
