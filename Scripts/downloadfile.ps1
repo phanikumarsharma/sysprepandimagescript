303fd5a9dad9b39bfb8acf5cadb0f9b609888da4
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-13 11:08:43 +0530

    Update downloadfile.ps1

--- Scripts/downloadfile.ps1
+++ Scripts/downloadfile.ps1
@@ -1,5 +1,5 @@
-$output = "C:\Program Files\ODT\odt.exe"
+$url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
+$output = "D:\Program Files\ODT\odt.exe"
 
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
-
