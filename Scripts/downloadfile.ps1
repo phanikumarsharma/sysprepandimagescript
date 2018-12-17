9f3f5c80ae234dbb78b1f06ff07abad56ec99d81
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-17 12:00:15 +0530

    Update downloadfile.ps1

--- Scripts/downloadfile.ps1
+++ Scripts/downloadfile.ps1
@@ -1,9 +1,9 @@
 $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
-$output = "C:\Program Files\odt.exe"
+$output = "$env:Temp\odt.exe"
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
 #to extract the ODT tool
-Start-Process -FilePath "C:\Program Files\odt.exe" -ArgumentList '/extract:"C:\Program Files\O365" /quiet'
-Start-Job -ScriptBlock { Start-Process -FilePath "C:\Program Files\O365\setup.exe" -ArgumentList '/configure "C:\Program Files\O365\configuration-Office365-x64.xml"'}
-#Start-Process -FilePath "C:\Program Files\O365\setup.exe" -ArgumentList '/configure "C:\Program Files\O365\configuration-Office365-x64.xml"'
+Start-Process -FilePath "$env:Temp\odt.exe" -ArgumentList '/extract:"$env:Temp\O365" /quiet'
+#Start-Job -ScriptBlock { Start-Process -FilePath "$env:Temp\O365\setup.exe" -ArgumentList '/configure "$env:Temp\O365\configuration-Office365-x64.xml"'}
+Start-Process -FilePath "$env:Temp\O365\setup.exe" -ArgumentList '/configure "$env:Temp\O365\configuration-Office365-x64.xml"'
 
