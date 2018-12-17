2db0cc674f18114775c2fea27d6b0b48547e1574
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-17 18:18:21 +0530

    Update downloadfile.ps1

--- Scripts/downloadfile.ps1
+++ Scripts/downloadfile.ps1
@@ -5,7 +5,9 @@ Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
 #to extract the ODT tool
 Start-Process -FilePath "$env:Temp\odt.exe" -ArgumentList '/extract:"D:/O365" /quiet'
-Invoke-Expression -Command "cmd.exe /c D:/O365/setup.exe /configure 'D:/O365/configuration-Office365-x64.xml'"
+Invoke-Expression -Command "cmd.exe /c 'D:/O3652019/setup.exe' /download 'D:/O3652019/configuration-Office365-x64.xml'" 
+Invoke-Expression -Command "cmd.exe /c 'D:/O3652019/setup.exe' /configure 'D:/O3652019/configuration-Office365-x64.xml' /S" 
+#Invoke-Expression -Command "cmd.exe /c D:/O365/setup.exe /configure 'D:/O365/configuration-Office365-x64.xml'"
 
 #Start-Job -ScriptBlock { Start-Process -FilePath "$env:Temp\O365\setup.exe" -ArgumentList '/configure "$env:Temp\O365\configuration-Office365-x64.xml"'}
 
