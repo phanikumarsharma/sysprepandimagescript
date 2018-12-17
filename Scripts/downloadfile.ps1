cac321b3dba910eef83ef843f113ebfdcec92158
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-17 15:25:24 +0530

    Update downloadfile.ps1

--- Scripts/downloadfile.ps1
+++ Scripts/downloadfile.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
 $output = "$env:Temp\odt.exe"
 Import-Module BitsTransfer
