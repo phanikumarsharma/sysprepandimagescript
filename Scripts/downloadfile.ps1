3a99694c645206596ab338dea98845ec1b7165a7
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-18 18:15:15 +0530

    Update downloadfile.ps1

--- Scripts/downloadfile.ps1
+++ Scripts/downloadfile.ps1
@@ -4,11 +4,11 @@ Invoke-Expression -Command "cmd.exe /c .\Setup.exe /configure D:\O365\configurat
 
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
-$output = "D:\odt.exe"
+$output = "D:/odt.exe"
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
 #to extract the ODT tool
-Start-Process -FilePath "D:\odt.exe" -ArgumentList '/extract:"D:\O365" /quiet'
+Start-Process -FilePath "D:/odt.exe" -ArgumentList '/extract:"D:/O365" /quiet'
 
 Set-Location "D:/O365"
 Invoke-Expression -Command "cmd.exe /c '.\setup.exe' /download 'D:/O365/configuration-Office365-x64.xml'" 
