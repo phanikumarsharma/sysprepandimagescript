925089f76a0ac3ef8b645e44f9e1be2c2da99c63
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-21 14:36:59 +0530

    Update office2019.ps1

--- Scripts/office2019.ps1
+++ Scripts/office2019.ps1
@@ -1,4 +1,4 @@
-$url = "https://unmstrgaccnt9.blob.core.windows.net/iso/en_office_professional_plus_2019_x86_x64_dvd_7ea28c99%20(1).iso"
+$url = "https://office19rgdisks.blob.core.windows.net/office19-iso/en_office_professional_plus_2019_x86_x64_dvd_7ea28c99.iso"
 $output = "C:\office.iso"
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
