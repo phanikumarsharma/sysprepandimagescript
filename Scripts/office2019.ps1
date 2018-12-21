6f1cbebb1daa9bd9f4b4f377f598250aedb6981a
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-21 15:32:07 +0530

    Update office2019.ps1

--- Scripts/office2019.ps1
+++ Scripts/office2019.ps1
@@ -3,7 +3,7 @@ $output = "C:\office.iso"
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
 Mount-DiskImage -ImagePath "C:\office.iso"
-Set-Location "E:\Office"
+Set-Location "F:\Office"
 Start-Process -FilePath ".\Setup64.exe"
 
 
