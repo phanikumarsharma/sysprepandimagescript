4536e5fb58536edded91c190873b7638ecef1775
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-26 15:41:23 +0530

    Update function.ps1

--- Scripts/function.ps1
+++ Scripts/function.ps1
@@ -14,9 +14,14 @@ Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /configure 'D:\O365\c
 office365Installation
 #>
 function RunSysprep{
+param([switch]$runSysprep=$false)
+write-output "Sysprep Script Run, parameter 'runSysprep': $runSysprep"
 if($runSysprep){
   write-output "starting the Sysprep"
   Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /shutdown /quiet'
+  write-output "started Sysprep"
+}else{
+  write-output "skipping the Sysprep"
 }
 RunSysprep
 
