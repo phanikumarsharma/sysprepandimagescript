3abddcef4f14c1a4393831783bc5818dad83c69f
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-26 14:52:25 +0530

    Update function.ps1

--- Scripts/function.ps1
+++ Scripts/function.ps1
@@ -1,19 +1,18 @@
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
-function office365Installation{
+function office365Installation
+{
 $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
 $output = "D:/odt.exe"
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
 #to extract the ODT tool
 Start-Process -FilePath "D:/odt.exe" -ArgumentList '/extract:"D:/O365" /quiet'
-Set-Location "D:/O365"
-Invoke-Expression -Command "cmd.exe /c '.\setup.exe' /download 'D:\O365\configuration-Office365-x64.xml'" 
+Set-Location "D:\O365"
+#Invoke-Expression -Command "cmd.exe /c '.\setup.exe' /download 'D:\O365\configuration-Office365-x64.xml'" 
 Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /configure 'D:\O365\configuration-Office365-x64.xml'" 
-
 }
-
 office365Installation
-
+<#
 param([switch]$runSysprep=$false)
 function RunSysprep{
 if($runSysprep){
@@ -26,3 +25,4 @@ if($runSysprep){
 }
 
 RunSysprep
+#>
