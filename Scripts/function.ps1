7e295778fd946a3127822cbe5cfec05b1c06789c
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-27 14:09:06 +0530

    Update function.ps1

--- Scripts/function.ps1
+++ Scripts/function.ps1
@@ -1,4 +1,21 @@
+
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+Start-Job -Name Job1 -ScriptBlock {$url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
+$output = "D:/odt.exe"
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url -Destination $output
+#to extract the ODT tool
+Start-Process -FilePath "D:/odt.exe" -ArgumentList '/extract:"D:/O365" /quiet'
+Set-Location "D:\O365"
+Invoke-Expression -Command "cmd.exe /c '.\setup.exe' /download 'D:\O365\configuration-Office365-x64.xml'" 
+Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /configure 'D:\O365\configuration-Office365-x64.xml'" 
+}
+Wait-Job -Name Job1
+Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /shutdown /quiet'
+
+
+
+<#Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 function office365Installation{
 $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
 $output = "D:/odt.exe"
@@ -15,4 +32,4 @@ function RunSysprep{
   Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /shutdown /quiet'
 }
 RunSysprep
-
+#>
