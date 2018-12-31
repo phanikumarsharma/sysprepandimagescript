daafb29e42f9d0a09745fb4427d9b5e670f0c37c
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-31 12:48:03 +0530

    Update function.ps1

--- Scripts/function.ps1
+++ Scripts/function.ps1
@@ -1,21 +1,6 @@
-
+function O365
+{
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
-Start-Job -Name Job1 -ScriptBlock {$url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
-$output = "D:/odt.exe"
-Import-Module BitsTransfer
-Start-BitsTransfer -Source $url -Destination $output
-#to extract the ODT tool
-Start-Process -FilePath "D:/odt.exe" -ArgumentList '/extract:"D:/O365" /quiet'
-Set-Location "D:\O365"
-Invoke-Expression -Command "cmd.exe /c '.\setup.exe' /download 'D:\O365\configuration-Office365-x64.xml'" 
-Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /configure 'D:\O365\configuration-Office365-x64.xml'" 
-}
-Wait-Job -Name Job1
-Start-Job -Name Job2 -ScriptBlock {Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /shutdown /quiet'}
-Wait-Job -Name Job2
-Stop-Job -Name "Job2"
-<#Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
-function office365Installation{
 $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
 $output = "D:/odt.exe"
 Import-Module BitsTransfer
@@ -26,20 +11,16 @@ Set-Location "D:/O365"
 Invoke-Expression -Command "cmd.exe /c '.\setup.exe' /download 'D:\O365\configuration-Office365-x64.xml'" 
 Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /configure 'D:\O365\configuration-Office365-x64.xml'" 
 }
-office365Installation
-function RunSysprep{
-  Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /shutdown /quiet'
+function sysprep
+{
+$checkinstalled = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -eq "Microsoft Office 365 ProPlus - en-us"} | Select-Object DisplayName
+if($checkinstalled)
+{
+Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /shutdown /quiet'
 }
-RunSysprep
-
-
-$url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
-$output = "D:/odt.exe"
-Import-Module BitsTransfer
-Start-BitsTransfer -Source $url -Destination $output
-#to extract the ODT tool
-Start-Process -FilePath "D:/odt.exe" -ArgumentList '/extract:"D:/O365" /quiet'
-Set-Location "D:\O365"
-Invoke-Expression -Command "cmd.exe /c '.\setup.exe' /download 'D:\O365\configuration-Office365-x64.xml'" 
-Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /configure 'D:\O365\configuration-Office365-x64.xml'" 
-#>
+}
+else
+{
+O365
+}
+sysprep
