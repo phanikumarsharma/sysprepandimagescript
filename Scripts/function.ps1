fd77784f3012ef573a372d3d713084688dfc3394
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-31 14:14:55 +0530

    Update function.ps1

--- Scripts/function.ps1
+++ Scripts/function.ps1
@@ -7,7 +7,7 @@ Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
 #to extract the ODT tool
 Start-Process -FilePath "D:/odt.exe" -ArgumentList '/extract:"D:/O365" /quiet'
-Set-Location "D:/O365"
+Set-Location "D:\O365"
 Invoke-Expression -Command "cmd.exe /c '.\setup.exe' /download 'D:\O365\configuration-Office365-x64.xml'" 
 Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /configure 'D:\O365\configuration-Office365-x64.xml'" 
 }
@@ -18,10 +18,6 @@ $checkinstalled = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersi
 if($checkinstalled)
 {
 Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /shutdown /quiet'
-}
-else
-{
-office365Installation
 }
 }
 sysprep
