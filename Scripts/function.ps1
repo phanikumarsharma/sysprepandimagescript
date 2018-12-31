6e63e9194f84dddf937ad3fcda10c9fd1c0dc4f4
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-12-31 12:49:06 +0530

    Update function.ps1

--- Scripts/function.ps1
+++ Scripts/function.ps1
@@ -1,4 +1,4 @@
-function O365
+function office365Installation
 {
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
@@ -21,6 +21,6 @@ Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/
 }
 else
 {
-O365
+office365Installation
 }
 sysprep
