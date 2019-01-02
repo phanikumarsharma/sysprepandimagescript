9d6f845c34d04ce1a03a1c85a4795c7eb59b4007
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2019-01-02 14:48:59 +0530

    Update function.ps1

--- Scripts/function.ps1
+++ Scripts/function.ps1
@@ -1,27 +1,28 @@
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 function O365
 {
+New-Item -Path "D:\" -ItemType Directory -Force -ErrorAction SilentlyContinue
 $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
 $output = "D:/odt.exe"
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
 #to extract the ODT tool
-New-Item -Path "D:\O365" -ItemType Directory -Force -ErrorAction SilentlyContinue
 Start-Process -FilePath "D:/odt.exe" -ArgumentList '/extract:"D:/O365" /quiet'
 Set-Location "D:\O365"
 Invoke-Expression -Command "cmd.exe /c '.\setup.exe' /download 'D:\O365\configuration-Office365-x64.xml'" 
 Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /configure 'D:\O365\configuration-Office365-x64.xml'" 
 }
 Out-Null | O365
-function second
+function sysprep
 {
 $checkinstalled=Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -eq "Microsoft Office 365 ProPlus - en-us"}
 if($checkinstalled)
 {
-Write-Host "yes this cmd executes after installation of O365"
+
+  Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /shutdown /quiet'
 }
 }
-second
+Out-Null | sysprep
 <#
 function office365Installation
 {
