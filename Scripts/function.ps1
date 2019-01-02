ea5193ef46dc845f8a2bc25b0e828849d641a02a
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2019-01-02 15:36:08 +0530

    Update function.ps1

--- Scripts/function.ps1
+++ Scripts/function.ps1
@@ -6,8 +6,8 @@ $output = "D:/odt.exe"
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
 #to extract the ODT tool
-Start-Process -FilePath "D:/odt.exe" -ArgumentList '/extract:"D:/O365" /quiet'
 New-Item -Path "D:\O365" -ItemType Directory -Force -ErrorAction SilentlyContinue
+Start-Process -FilePath "D:/odt.exe" -ArgumentList '/extract:"D:/O365" /quiet'
 Set-Location D:\O365
 #Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /download 'D:\O365\configuration-Office365-x64.xml'" 
 Invoke-Expression -Command "cmd.exe /c D:\O365\setup.exe /configure D:\O365\configuration-Office365-x64.xml" 
