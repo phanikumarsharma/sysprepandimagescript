ca9b34a9e84dd4443643ec4e1e57174f628534a1
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2019-01-02 13:11:07 +0530

    Update function.ps1

--- Scripts/function.ps1
+++ Scripts/function.ps1
@@ -2,14 +2,14 @@ function office365Installation
 {
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
-$output = "C:/odt.exe"
+$output = "D:/odt.exe"
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
 #to extract the ODT tool
-Start-Process -FilePath "C:/odt.exe" -ArgumentList '/extract:"C:/O365" /quiet'
-Set-Location "C:\O365"
-Invoke-Expression -Command "cmd.exe /c '.\setup.exe' /download 'C:\O365\configuration-Office365-x64.xml'" 
-Invoke-Expression -Command "cmd.exe /c 'C:\O365\setup.exe' /configure 'C:\O365\configuration-Office365-x64.xml'" 
+Start-Process -FilePath "D:/odt.exe" -ArgumentList '/extract:"D:/O365" /quiet'
+Set-Location "D:\O365"
+Invoke-Expression -Command "cmd.exe /c '.\setup.exe' /download 'D:\O365\configuration-Office365-x64.xml'" 
+Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /configure 'D:\O365\configuration-Office365-x64.xml'" 
 }
 Out-Null | office365Installation
 function second
