d52c8a80ebcfdd19408f2f5855ca77f2f43a923e
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2019-01-09 11:26:24 +0530

    Update office2019.ps1

--- Scripts/office2019.ps1
+++ Scripts/office2019.ps1
@@ -1,4 +1,4 @@
-    function O365
+    function Office2019
     {
     $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
     $output = "D:/odt.exe"
@@ -6,10 +6,10 @@
     Start-BitsTransfer -Source $url -Destination $output
     #to extract the ODT tool
     New-Item -Path "D:\Office2019" -ItemType Directory -Force -ErrorAction SilentlyContinue
-    Start-Process -FilePath "D:/odt.exe" -ArgumentList '/extract:"D:/O365" /quiet'
+    Start-Process -FilePath "D:/odt.exe" -ArgumentList '/extract:"D:/Office2019" /quiet'
     Start-Sleep -Seconds 15
-    Set-Location "D:\O365"
-    Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /download 'D:\O365\configuration-Office365-x64.xml'" 
-    Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /configure 'D:\O365\configuration-Office365-x64.xml'" 
+    Set-Location "D:\Office2019"
+    Invoke-Expression -Command "cmd.exe /c 'D:\Office2019\setup.exe' /download 'D:\Office2019\configuration-Office2019Enterprise.xml'" 
+    Invoke-Expression -Command "cmd.exe /c 'D:\Office2019\setup.exe' /configure 'D:\Office2019\configuration-Office2019Enterprise.xml'" 
     }
-    Out-Null | O365
+    Out-Null | Office2019
