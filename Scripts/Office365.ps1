2763947f2233ea246ce90944057c251067a8f429
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2020-01-02 17:05:22 +0530

    Update Office365.ps1

--- Scripts/Office365.ps1
+++ Scripts/Office365.ps1
@@ -1,44 +1,3 @@
-#Script to setup golden image with Azure Image Builder
-
-
-#Create temp folder
-New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
-
-
-#Install VSCode
-Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?Linkid=852157' -OutFile 'c:\temp\VScode.exe'
-Invoke-Expression -Command 'c:\temp\VScode.exe /verysilent'
-
-#Start sleep
-Start-Sleep -Seconds 10
-
-#InstallNotepadplusplus
-Invoke-WebRequest -Uri 'https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.x64.exe' -OutFile 'c:\temp\notepadplusplus.exe'
-Invoke-Expression -Command 'c:\temp\notepadplusplus.exe /S'
-
-#Start sleep
-Start-Sleep -Seconds 10
-
-#InstallFSLogix
-Invoke-WebRequest -Uri 'https://aka.ms/fslogix_download' -OutFile 'c:\temp\fslogix.zip'
-Start-Sleep -Seconds 10
-Expand-Archive -Path 'C:\temp\fslogix.zip' -DestinationPath 'C:\temp\fslogix\'  -Force
-Invoke-Expression -Command 'C:\temp\fslogix\x64\Release\FSLogixAppsSetup.exe /install /quiet /norestart'
-
-    
-$url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_12130-20272.exe"
-$output = 'C:\temp\odt.exe'
-Import-Module BitsTransfer
-Start-BitsTransfer -Source $url -Destination $output
-#to extract the ODT tool
-New-Item -Path "C:\temp\O365" -ItemType Directory -Force -ErrorAction SilentlyContinue
-Start-Process -FilePath "C:\temp\odt.exe" -ArgumentList '/extract:"C:\temp\O365" /quiet'
-Start-Sleep -Seconds 15
-Set-Location "C:\temp\O365"
-Invoke-Expression -Command "cmd.exe /c 'C:\temp\O365\setup.exe' /download 'C:\temp\O365\configuration-Office365-x64.xml'" 
-Invoke-Expression -Command "cmd.exe /c 'C:\temp\O365\setup.exe' /configure 'C:\temp\O365\configuration-Office365-x64.xml'" 
-
-
 <#
 
 .Synposys
@@ -50,17 +9,15 @@ This script is used to install the Office 365 and to enable the WinRm.
 .Permission
 Administrator
 #>
-#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
-<#
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
 $dnsName= (Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain
 Enable-PSRemoting
 $Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName $dnsName
 Export-Certificate -Cert $Cert -FilePath 'C:\exch.cer'
 New-Item -Path "WSMan:\LocalHost\Listener" -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
 New-NetFirewallRule -Name "winrm_https" -DisplayName "winrm_https" -Enabled True -Profile Any -Action Allow -Direction Inbound -LocalPort 5986 -Protocol TCP
-#>
 # Install O365
-<#function O365
+function O365
 {
 $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
 $output = "D:/odt.exe"
@@ -74,15 +31,14 @@ Set-Location "D:\O365"
 Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /download 'D:\O365\configuration-Office365-x64.xml'" 
 Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /configure 'D:\O365\configuration-Office365-x64.xml'" 
 }
-Out-Null | O365#>
-<#
+Out-Null | O365
+
 Enable-PSRemoting
 $Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName $dnsName
 Export-Certificate -Cert $Cert -FilePath 'C:\exch.cer'
 New-Item -Path "WSMan:\LocalHost\Listener" -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
 New-NetFirewallRule -Name "winrm_https" -DisplayName "winrm_https" -Enabled True -Profile Any -Action Allow -Direction Inbound -LocalPort 5986 -Protocol TCP
-#>
-<#
+
 function sysprep
 {
 $checkinstalled=Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -eq "Microsoft Office 365 ProPlus - en-us"}
@@ -92,5 +48,5 @@ Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/
 }
 }
 Out-Null | sysprep
-#>
+
 
